ddb006ff23dd5852a2168a2126560c412b71e31a
Guomin Huang <hguomin@outlook.com>
2022-05-05 10:00:33 +0800

    Initial project code

---
+++ src/connex-cloud/application/src/main/java/dev/iotapp/connex/application/ConnexCloudApplication.java
@@ -0,0 +1,13 @@
+package dev.iotapp.connex.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnexCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnexCloudApplication.class, args);
+	}
+
+}
---
+++ src/connex-cloud/application/src/test/java/dev/iotapp/connex/connexcloudapps/ConnexCloudappsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iotapp.connex.connexcloudapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexCloudappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
